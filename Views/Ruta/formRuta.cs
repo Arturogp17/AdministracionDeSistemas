06d93b051f1c15cc2c30efa1367c23d4533d9a87
Ivan Rojas <ivanrojas97@icloud.com>
2021-03-18 22:38:01 -0600

    Forms para Ruta

---
+++ Views/Ruta/formRuta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoABD.Views.Ruta
+{
+    public partial class formRuta : Form
+    {
+        public formRuta()
+        {
+            InitializeComponent();
+        }
+    }
+}
